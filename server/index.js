20726154552307c85779bef9fa10be64e424e5ed
Nilam Chavda <nilamvadher@gmail.com>
2023-03-20 17:48:46 -0700

    created routes

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log('hello world')
+// console.log('hello world')
+
+var express = require('express');
+var app = express();
+
+app.get('/nilu', function(req, res){
+    res.send("I am available here");
+
+})
+
+app.post('/nilu', function(req, res){
+    res.send("I am available in post also");
+
+})
+
+var server = app.listen(8080)
+
+
